ff82b1030bdff9ad91fbed2d07c4054793be0459
Francisco Jair <fjair123@ufpi.edu.br>
2020-08-31 12:15:34 -0300

    Criado os componetes para o order

---
+++ src/controllers/order_controller.js
@@ -0,0 +1,24 @@
+const repository = require('../repositorios/order_repositorio')
+const guid = require('guid')
+
+exports.get = async (req, res, next) => {
+    try {
+        var data = await repository.get()
+        res.status(200).send(data)
+    } catch (e) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição' })
+    }
+}
+
+exports.post = async (req, res, next) => {
+    try {
+        await repository.create({
+            customer: req.body.customer,
+            number: guid.raw().substring(0, 6),
+            items: req.body.items
+        })
+        res.status(201).send({ message: "Pedido cadastrado com sucesso" });
+    } catch (error) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição' })
+    }
+}
---
+++ src/routers/order_route.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const controller = require('../controllers/order_controller')
+
+const router = express.Router();
+
+router.get('/', controller.get)
+router.post('/', controller.post)
+
+module.exports = router;
